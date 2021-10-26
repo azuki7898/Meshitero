2cf50c765fbe41997a55bfe9af359a43967d0fee
azuki7898 <hina.apoo.0124@gmail.com>
2021-10-26 12:30:20 +0000

    [Finish]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fdf55def372e4587ba2e445c387a21dd8bcfa2e3464fd634e67e835f3bd0f411db2e4262792c75b91f2006e0a9ad973a561077be885197d198ee846e0544d9e0'
